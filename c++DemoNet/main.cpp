10bbcaae4f67721ef7b8098c2d5c03d87e63a806
aghanoum <aghanoum@10.150.32.226>
2022-09-06 13:42:19 +0200

    Push 12

---
+++ c++DemoNet/main.cpp
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/main.cc to edit this template
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: aghanoum
+ *
+ * Created on 6 September 2022, 11:20
+ */
+
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+   cout << "hier ist C++ Demo bitte push it once again !" << endl;
+    return 0;
+}

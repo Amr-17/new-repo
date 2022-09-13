2e3f2ec5d5bfd58c9cd2786b3a8b90ce4b1e4669
aghanoum <aghanoum@DESKTOP-8PKU4C8>
2022-09-13 13:36:01 +0200

    First Project for getenv

---
+++ Project_getenv/Pro_getenv.cpp
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/main.cc to edit this template
+ */
+
+/* 
+ * File:   Pro_getenv.cpp
+ * Author: aghanoum
+ *
+ * Created on 13 September 2022, 13:20
+ */
+
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+	/* Eine liste von möglichen Umgebungvariables*/
+	const char *env_var[2] = {"PUBLIC","HOME"}; /*  Intialiesieren von string mit drei Elementen    */
+	char *env_val[2];
+
+	for(int i=0; i<2; i++)
+	{
+		/* Einen Value bekommen für die Umgebungsvariablen falls Vorhanden */
+		env_val[i] = getenv(env_var[i]);
+		if (env_val[i] != NULL)
+			cout << "Variable = " << env_var[i] << ", Value= " << env_val[i] << endl;
+		else
+			cout << env_var[i] << " doesn't exist" << endl;
+	}
+}
---
+++ Project_getenv/build/Debug/MinGW-Windows/Pro_getenv.o.d
@@ -0,0 +1,1 @@
+build/Debug/MinGW-Windows/Pro_getenv.o: Pro_getenv.cpp
